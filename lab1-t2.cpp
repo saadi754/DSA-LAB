5248431f1dc5ba6dd977735f28e3ab8cfe1aa0fe
saadi754 <saadkarim775544@gmail.com>
2026-02-09 05:07:28 +0000

    sidafbpuiasbdfposueah

--- lab1-t2.cpp
+++ lab1-t2.cpp
@@ -1,22 +1,39 @@
 #include<iostream>
 using namespace std;
 
-struct afnan{
-    int a;
-    int b;
+struct student{
+    int roll;
+    char name[20];
+    float marks;
+    float percentage;
+    string grade;
+    char caste[20];
 };
 
-void add(struct afnan *n){
-    cout<<"a = ";
-    cin>>n->a;
-    cout<<"b = ";
-    cin>>n->b;
-    cout<<"the sum is "<<n->a+n->b<<endl;
-}
-
-
 int main(){
-    struct afnan n1;
-    add(&n1);
-    return 0;
+    student s1;
+    student *ptr=&s1;
+
+    cout<<"enter the roll number = ";
+    cin>>ptr->roll;
+    cout<<"enter the name = ";
+    cin>>ptr->name;
+    cout<<"enter the marks = ";
+    cin>>ptr->marks;
+    cout<<"enter the percentage = ";
+    cin>>ptr->percentage;
+    cout<<"enter the grade = ";
+    cin>>ptr->grade;
+    cout<<"enter the caste = ";
+    cin>>ptr->caste;
+
+    cout<<"the details of student are "<<endl;
+    cout<<"roll number = "<<ptr->roll<<endl;
+    cout<<"name = "<<ptr->name<<endl;
+    cout<<"marks = "<<ptr->marks<<endl;
+    cout<<"percentage = "<<ptr->percentage<<endl;
+    cout<<"grade = "<<ptr->grade<<endl;
+    cout<<"caste = "<<ptr->caste<<endl;
+
+    return 0; 
 }
